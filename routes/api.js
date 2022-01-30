f411c01846e97e2ee188cee531c8be8b26e02e1e
Alex Widener <awidener3@gmail.com>
2022-01-30 14:17:12 -0500

    fixed indentation and added comments

--- routes/api.js
+++ routes/api.js
@@ -1,48 +1,62 @@
 const router = require('express').Router();
-const { readAndAppend, readFromFile, writeToFile } = require('../helpers/fsUtils.js')
 const { v4: uuidv4 } = require('uuid');
 const fs = require('fs');
-const { parse } = require('path');
-const { request } = require('http');
+const {
+	readAndAppend,
+	readFromFile,
+	writeToFile,
+} = require('../helpers/fsUtils.js');
 
 router.get('/notes', (req, res) => {
-    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
-})
+	// Send data from db
+	readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
 
 router.post('/notes', (req, res) => {
-    // res.json(req.body)
-    const { title, text } = req.body;
+	// Create new db entry
+	const { title, text } = req.body;
+	if (title && text) {
+		const newNote = {
+			title,
+			text,
+			id: uuidv4(),
+		};
+		readAndAppend(newNote, './db/db.json');
 
-    if (title && text) {
-        const newNote = {
-            title,
-            text,
-            id: uuidv4(),
-        }
-        readAndAppend(newNote, './db/db.json');
-    
-        const response = {
-            status: 'success',
-            body: newNote
-        }
-    
-        res.json(response);
-    } else {
-        res.json('Error in adding note');
-    }
-})
+		// Send response
+		const response = {
+			status: 'success',
+			body: newNote,
+		};
+		res.json(response);
+	} else {
+		// Throw error
+		res.json('Error in adding note');
+	}
+});
 
 router.delete('/notes/:id', (req, res) => {
-    const notes = JSON.parse(fs.readFileSync('./db/db.json'));
+	// Get data
+	const notes = JSON.parse(fs.readFileSync('./db/db.json'));
 
-    const note = notes.find(note => note.id === req.params.id)
-    if (!note) return res.status(404).send(`Sorry! There is no course with the id ${req.params.id}`);
+	// Find the note
+	const note = notes.find((note) => note.id === req.params.id);
+	if (!note)
+		return res
+			.status(404)
+			.send(`Sorry! There is no course with the id ${req.params.id}`);
 
-    const index = notes.indexOf(note);
-    notes.splice(index, 1);
-    writeToFile('./db/db.json', notes);
+	// Remove from db
+	const index = notes.indexOf(note);
+	notes.splice(index, 1);
+	writeToFile('./db/db.json', notes);
 
-    res.send(note)
-})
+	// Send response
+	const response = {
+		status: 'success',
+		body: note,
+	};
+	res.json(response);
+});
 
 module.exports = router;
